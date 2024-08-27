276d9c69f9f487a1a14d03df36a69b7aa829599f
plyr0 <9119209+plyr0@users.noreply.github.com>
2024-08-27 14:59:45 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace BiblionetkaScraper2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var output = Path.GetDirectoryName(args[0]) + Path.DirectorySeparatorChar + Path.GetFileNameWithoutExtension(args[0]) + ".csv";
+            foreach(var a in args)
+                Scrap(a, output);
+        }
+
+        private static void Scrap(string inputFile, string outputFile)
+        {
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.Load(inputFile);
+
+            var notes = doc.DocumentNode.SelectSingleNode("/html/body/form/div[3]/section[4]/div[2]/div[1]/div[2]/div/div/div[5]")
+                .Descendants()
+                .Where(node => node.GetAttributeValue("class", "").Contains("row forum__list"))
+                .ToList();
+
+            var path = outputFile;
+            using var file = File.Exists(path) ? File.Open(path, FileMode.Append) : File.Open(path, FileMode.CreateNew);
+            using var stream = new StreamWriter(file);
+            foreach (var n in notes)
+            {
+                var note = n.SelectSingleNode("div[1]/a[1]");
+                var bookName = '"' + note.InnerText.Replace("\"", "'") + '"';
+
+                var bookNote = note.GetAttributeValue("title", "").Replace("Twoja ocena: ", "").Replace(",", ".");
+                
+                var authors = n.SelectNodes("div[1]/a")
+                    .Skip(1)
+                    .Where(node => !node.GetAttributeValue("class", "").Contains("icon"))
+                    .Select(n => n.InnerText);
+
+                var authorsJoined = authors.Count() > 1 ? string.Join(",", authors) : authors.FirstOrDefault();
+                var bookAuthors = '"' + authorsJoined + '"';
+
+                string line = bookName + "," + bookAuthors + "," + bookNote;
+                Console.WriteLine(line);
+                stream.WriteLine(line);
+            }
+        }
+    }
+}
